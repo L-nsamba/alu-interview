d4a54c45e8f58af609ecc4d2ef9f807ad7901eb7
L-nsamba <l.nsamba@alustudent.com>
2026-02-25 12:02:00 +0200

    pascal_triangle/

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal’s triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        # Start each row with 1
+        row = [1]
+        # Fill in the middle values
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        # End each row with 1
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
